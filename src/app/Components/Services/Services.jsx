9418bc440c47982021841227a66e292f95e0cc59
DIGI INDIA SOLUTIONS <digiindiasolutions2023@gmail.com>
2025-11-17 19:12:38 +0530

    first H J

--- src/app/Components/Services/Services.jsx
+++ src/app/Components/Services/Services.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import "./ServicesSection.css";
 
 import pic1 from "@/app/images/Service.png";
-import Pic2 from "../../images/Service2.png";
+import Pic2 from "../../images/service2.png";
 import pic3 from "@/app/images/Service3.png";
 import pic4 from "@/app/images/Service4.png";
 import pic5 from "@/app/images/Service5.png";
